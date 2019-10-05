08ab3d632b28a518f27860c1790872d8e9919109
linkouille <linkouillou@gmail.com>
2019-10-05 13:53:57 +0200

    ItemFolow corrigé

--- Assets/Scripts/Robot/ItemFollow.cs
+++ Assets/Scripts/Robot/ItemFollow.cs
@@ -8,9 +8,7 @@ public class ItemFollow : MonoBehaviour
     [SerializeField]
     private GameObject Target;
     
-    public Vector3 offset_aux = new Vector3(0, 0, 0);
     private Vector3 Direction;
-    private Vector3 Dist_min = new Vector3(1, 1, 1);
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -19,23 +17,20 @@ public class ItemFollow : MonoBehaviour
             Target = collision.gameObject;
         }
     }
-    // Update is called once per frame
+
+    [SerializeField]private float distMax;
+
     void Update()
     {
         if (Target != null)
         {
             Direction = Target.transform.position - transform.position;
-            if ( Vector3.Distance(Target.transform.position, this.transform.position) > 1.50f || Vector3.Distance(Target.transform.position, this.transform.position) > 1.51f)
+            if ( Vector3.Distance(Target.transform.position, transform.position) > distMax)
             {
 
-                transform.Translate(Direction * 1);
-                transform.position = Target.transform.position + offset_aux;
+                transform.Translate(Direction * Time.deltaTime);
 
             }
-        }
-        else
-        {
-            
         }
 
 
