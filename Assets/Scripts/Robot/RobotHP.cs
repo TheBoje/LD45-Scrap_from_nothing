a643d503a0c78cabccd9b51811d337e7fae996a1
Vincent <vincent.commin@etu.univ-poitiers.fr>
2019-10-05 22:30:27 +0200

    ajout des de la vie dans l'ui et correction de RobotHP.cs

--- Assets/Scripts/Robot/RobotHP.cs
+++ Assets/Scripts/Robot/RobotHP.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class RobotHP : MonoBehaviour
 {
     public float health;
     public float initHeal;
--- Assets/Scripts/UIFarmingScript.cs
+++ Assets/Scripts/UIFarmingScript.cs
@@ -8,29 +8,48 @@ public class UIFarmingScript : MonoBehaviour
 
     [SerializeField]
     private Text timer;
+    [SerializeField]
+    private Text lifeP1;
+    [SerializeField]
+    private Text lifeP2;
+
+
+    private float currCountdownValue;
+
+    [SerializeField]
+    private GameObject player_1;
+    [SerializeField]
+    private GameObject player_2;
+
 
     // Start is called before the first frame update
     void Start()
     {
-         StartCoroutine(StartCountdown());
-   
+        StartCoroutine(StartCountdown());
+        print_life();
     }
 
-        private float currCountdownValue;
-        public IEnumerator StartCountdown(float countdownValue = 90)
+    // Affiche les points de vie de chaque players
+    public void print_life()
+    {
+        lifeP1.text = player_1.GetComponent<RobotStats>().vie.ToString();
+        lifeP2.text = player_2.GetComponent<RobotStats>().vie.ToString();
+    }
+
+    // Gère le compteur    
+    public IEnumerator StartCountdown(float countdownValue = 90)
+    {
+        currCountdownValue = countdownValue;
+        while (currCountdownValue > 0)
         {
-            currCountdownValue = countdownValue;
-            while (currCountdownValue > 0)
-            {
-                Debug.Log("Countdown: " + currCountdownValue);
-                yield return new WaitForSeconds(1.0f);
-                currCountdownValue--;
+            Debug.Log("Countdown: " + currCountdownValue);
+            yield return new WaitForSeconds(1.0f);
+            currCountdownValue--;
 
-            timer.text = currCountdownValue.ToString();
-            }
-
-           
+        timer.text = currCountdownValue.ToString();
         }
+                 
+    }
 
     // Update is called once per frame
     void Update()
