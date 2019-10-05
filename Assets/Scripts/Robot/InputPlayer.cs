7201aa66c5b383a6bfea6fb03e87f73f3f5580fa
linkouille <linkouillou@gmail.com>
2019-10-05 02:15:27 +0200

    Axis based Movement
    Axis based Movement and input handler

---
+++ Assets/Scripts/Robot/InputPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputPlayer : MonoBehaviour
+{
+    public int playerNumber;
+
+    public string axisH;
+    public string axisV;
+
+    private RobotMovement rM;
+
+    private void Awake()
+    {
+        rM = GetComponent<RobotMovement>();
+    }
+
+    private void Update()
+    {
+        rM.input = new Vector2(Input.GetAxis(axisH), Input.GetAxis(axisV));
+    }
+}
