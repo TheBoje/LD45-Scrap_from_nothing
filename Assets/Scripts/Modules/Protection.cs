ad2a798ef7f3a01ae4193b3b05b053d5b82a2d78
TheBoje <louis.leenar@gmail.com>
2019-10-06 02:11:56 +0200

    Scene louis

--- Assets/Scripts/Modules/Protection.cs
+++ Assets/Scripts/Modules/Protection.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 // Création du type énuméré relatif au type de protection du bouclier
-public enum TypeProtection { protect, reflect, laser };
+public enum TypeProtection { protect, reflect };
 
 public class Protection : Module
 {
 
     public TypeProtection type;
-    //public float angleDeProtection; Je ne vois pas comment l'implémenter pour l'instant
     public float defence;
+
+
 
 }
