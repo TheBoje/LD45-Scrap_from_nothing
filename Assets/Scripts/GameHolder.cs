a7292cb7ad9e6621e9127a6f2db84f72a4e912a1
Vincent <vincent.commin@etu.univ-poitiers.fr>
2019-10-06 12:06:34 +0200

    ajout du fichier pour le GameHolder

---
+++ Assets/Scripts/GameHolder.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHolder : MonoBehaviour
+{
+
+    private GameObject player_1;
+    private GameObject player_2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void farming_level()
+    {
+
+    }
+
+    public void combat_level()
+    {
+
+    }
+}
---
+++ Assets/Scripts/MenuChoixJoueurs.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuChoixJoueurs : MonoBehaviour
+{
+    private Sprite player_1;
+    private Sprite player_2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
