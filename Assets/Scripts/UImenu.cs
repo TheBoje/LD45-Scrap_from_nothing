dfc87fb51d850558fdb120643062f05f19ef2ba6
Vincent <vincent.commin@etu.univ-poitiers.fr>
2019-10-06 18:50:01 +0200

    menu boutons

---
+++ Assets/Scripts/UImenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UImenu : MonoBehaviour
+{
+
+    public void Options()
+    {
+        SceneManager.LoadScene("options");
+    }
+
+    public void Credits()
+    {
+
+    }
+
+    public void BTNStart()
+    {
+
+    }
+
+}
